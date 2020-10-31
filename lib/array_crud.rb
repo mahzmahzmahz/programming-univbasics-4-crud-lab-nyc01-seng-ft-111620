761b59c43e0bd8cfc7a31c5d094fedb780146f6e
Alex Marz <alexhmarz@gmail.com>
2020-10-31 21:36:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,16 +17,17 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["arrays!", "asa", "marz"]
+  array = ["wow", "asa", "arrays"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "asa", "arrays"]
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "am", "arrays"]
 end
 
 def retrieve_first_element_from_array(array)
